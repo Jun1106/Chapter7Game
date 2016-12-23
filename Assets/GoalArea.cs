a6309227be7e56b2474d7d97da4742f793294cfa
Junichi Ariyoshi <ariyoshi@toydea.com>
2016-12-24 00:13:17 +0900

    ゴール判定の仕込み

---
+++ Assets/GoalArea.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GoalArea : MonoBehaviour {
+	
+	bool isGoal = false;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider col) {
+		if (col.gameObject.tag == "Player") {
+			isGoal = true;
+		}
+	}
+}
